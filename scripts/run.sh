08b0179a62bd50678a35a6a630a350720515afc2
Andrey Kozichev <akozichev@gmail.com>
2023-03-08 17:01:41 +0000

    parametrised

---
+++ scripts/run.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+APP_FILE="{APP_FILE:=/opt/app/app.jar}"
+DB_HOST="{DB_HOST:=localhost}"
+DB_NAME="{DB_NAME:=workflow}"
+DB_USER="{DB_USER:=camunda}"
+DB_PASSWORD="{DB_PASSWORD:=camunda}"
+
+java -jar ${APP_FILE} -Dspring.datasource.url: jdbc:postgresql://${DB_HOST}:5432/${DB_NAME} -Dspring.datasource.username: ${DB_USER} -Dspring.datasource.password: ${DB_PASSWORD}
