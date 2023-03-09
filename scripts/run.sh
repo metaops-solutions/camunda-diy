36538bb00dc2d81fc2693f4b850c969207d43142
Andrey Kozichev <akozichev@gmail.com>
2023-03-09 11:15:18 +0000

    run script

--- scripts/run.sh
+++ scripts/run.sh
@@ -6,4 +6,4 @@ DB_NAME="${DB_NAME:=workflow}"
 DB_USER="${DB_USER:=camunda}"
 DB_PASSWORD="${DB_PASSWORD:=camunda}"
 
-java -jar ${APP_FILE} -Dspring.datasource.url: jdbc:postgresql://${DB_HOST}:5432/${DB_NAME} -Dspring.datasource.username: ${DB_USER} -Dspring.datasource.password: ${DB_PASSWORD}
+java -jar ${APP_FILE} -Dspring.datasource.url=jdbc:postgresql://${DB_HOST}:5432/${DB_NAME} -Dspring.datasource.username=${DB_USER} -Dspring.datasource.password=${DB_PASSWORD}
